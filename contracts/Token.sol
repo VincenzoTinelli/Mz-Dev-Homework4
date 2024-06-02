778722b0c02949c19393b489fd94886c9fc07382
VincenzoTinelli <vincenzotinelli1997@gmail.com>
2024-06-02 18:55:58 +0200

    Contratti inizializzati, posso procedere ai test

---
+++ contracts/Token.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+
+contract Token is ERC20Upgradeable, OwnableUpgradeable {
+    uint8 private VERSION;
+
+    function initialize(string memory _tokenName, string memory _tokenSymbol uint256 _supply) public initializer {
+        __ERC20_init_unchained(_tokenName, _tokenSymbol);
+        __Ownable_init_unchained();
+
+        _mint(msg.sender, _supply*(1e18));
+        VERSION = 1;
+    }
+
+    function getContractVersion() public view returns (uint8) {
+        return VERSION;
+    }
+}
---
+++ contracts/Token2.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "./Token.sol";
+import "./Tokwn2Storaage.sol";
+
+contract Token2 is ERC20Upgradeable, OwnableUpgradeable, Token2Storage {
+
+
+    function initialize(string memory _tokenName, string memory _tokenSymbol, uint256 _supply) public initializer {
+        __ERC20_init_unchained(_tokenName, _tokenSymbol);
+        __Ownable_init_unchained();
+
+        _mint(msg.sender, _supply*(1e18));
+    }
+
+    function setMaxSupply(uint256 _max) external onlyOwner {
+        require(_max * 1e18 >= totalSupply(), "Max supply cannot be less than total supply");
+        maxSupply = _max * 1e18;
+    }
+
+
+    function setVersion(uint8 _ver) external {
+        VERSION = _ver;
+    }
+}
